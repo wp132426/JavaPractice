3899d776dbf5368b61ee7cc23c4bb3fb7680accf
이건용 <@172.17.0.101>
2015-03-24 15:07:02 +0900

    as

--- src/org/dimigo/basic/Condition.java
+++ src/org/dimigo/basic/Condition.java
@@ -5,12 +5,15 @@ public class Condition {
 	public static void main(String[] args) {
 		
 		int speed = 10;
-		String type = "경차";
-		int pay=0;
+		String type = "고속버스";
+		int pay;
+		int plus_pay=200;
+		int mul=0;
 		
 		switch(type) {
 		case "고속버스":
 			pay = 850;
+			plus_pay=300;
 			break;
 		case "경차":
 			pay = 300;
@@ -19,9 +22,14 @@ public class Condition {
 			pay = 600;
 			break;
 		}
-		int mul = (speed/10);
+		int an=speed%10;
 		
-		pay = pay * mul;
+		if(an!=0)
+			mul=(speed/10);
+		else
+			mul=(speed/10)-1;
+		
+		pay += plus_pay * mul;
 		
 		System.out.println("<< 고속도로 통행료 계산 >>");
 		System.out.println("거리 : " + speed + "km");
